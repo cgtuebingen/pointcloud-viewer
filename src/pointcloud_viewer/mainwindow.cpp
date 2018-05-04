3cc2f309152448db4fbd104bdce0e0dd8e713585
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-05-04 12:36:02 +0200

    intializing opengl context

--- src/pointcloud_viewer/mainwindow.cpp
+++ src/pointcloud_viewer/mainwindow.cpp
@@ -10,23 +10,31 @@ extern GLFWwindow* glfw_window;
 
 void throw_glfw_error(int error, const char* description);
 
+// Constructor initializes glfw and an opengl 4.5 window
 Instance::Instance()
 {
+  // Initialize glfw
   if(glfwInit() != GLFW_TRUE)
     throw exception_t{"Couldn't initialize glfw"};
-
   glfwSetErrorCallback(throw_glfw_error);
 
+  // create a window with an opengl 4.5 context
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
   glfw_window = glfwCreateWindow(640, 480, "Pointcloud Viewer", nullptr, nullptr);
   if(!glfw_window)
     throw exception_t{"Couldn't create glfw window"};
+
+  glfwMakeContextCurrent(glfw_window);
 }
 
+// Constructor deinitializes glfw and destroays all windows
 Instance::~Instance()
 {
-  glfwTerminate();
-
+  glfwDestroyWindow(glfw_window);
   glfw_window = nullptr;
+
+  glfwTerminate();
 }
 
 GLFWwindow* glfw_window = nullptr;
