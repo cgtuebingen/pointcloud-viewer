5985fb8f7bef33bbbf3d657acee94c77ae687df5
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-05-12 13:52:01 +0200

    calling now glad to generate the headers

--- src/external/update_gl_headers.py
+++ src/external/update_gl_headers.py
@@ -3,7 +3,9 @@ import subprocess
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-os.chdir(script_dir)
-subprocess.call(['git', 'clone', 'https://github.com/dav1dde/glad.git', 'glad_script.git'])
+if not os.path.isdir('glad_script.git'):
+  subprocess.call(['git', 'clone', 'https://github.com/dav1dde/glad.git', 'glad_script.git'])
 
-
+os.chdir(os.path.join(script_dir, 'glad_script.git'))
+out_path = os.path.join(script_dir, 'GL')
+subprocess.call(['python', '-m', 'glad', '--profile=core', '--api', 'gl=4.5', '--generator=c', '--omit-khrplatform', '--out-path', out_path])
