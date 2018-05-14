4f123f5b7aa58cd89693bc34a10905e167369b17
Robert Hildebrandt <robert-42@web.de>
2018-05-14 16:07:05 +0200

    added LIKELY/UNLIKELY macros

---
+++ src/core_library/macros.hpp
@@ -0,0 +1,24 @@
+#ifndef CORELIBRARY_MACROS_HPP_
+#define CORELIBRARY_MACROS_HPP_
+
+#if defined(__GNUC__) || defined(__clang__)
+#define GCC_OR_CLANG 1
+#else
+#define GCC_OR_CLANG 0
+#endif
+
+
+
+#if GCC_OR_CLANG
+
+#define LIKELY(x) __builtin_expect(bool(x), 1)
+#define UNLIKELY(x) __builtin_expect(bool(x), 0)
+
+#else
+
+#define LIKELY(x)
+#define UNLIKELY(x)
+
+#endif
+
+#endif // CORELIBRARY_MACROS_HPP_
