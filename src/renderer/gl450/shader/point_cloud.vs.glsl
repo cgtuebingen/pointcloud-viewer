6adcc0a98c22366637ea3bb2c4a525d6c1c19d53
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-05-23 16:22:50 +0200

    using now the camera also for the point cloud renderer

--- src/renderer/gl450/shader/point_cloud.vs.glsl
+++ src/renderer/gl450/shader/point_cloud.vs.glsl
@@ -1,9 +1,11 @@
 #version 450 core
 
+#include <uniforms/global.vs.glsl>
+
 layout(location = POSITION_BINDING_INDEX)
 in vec4 point_coord;
 
 void main()
 {
-  gl_Position = vec4(point_coord.xyz, 1);
+  gl_Position = global.camera_matrix * vec4(point_coord.xyz, 1);
 }
