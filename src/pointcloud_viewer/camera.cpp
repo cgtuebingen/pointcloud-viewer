243e90756c9f53b35f61bdd8a5a30335db0333bd
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-05-23 12:48:35 +0200

    started actually implementing th ecamera

--- src/pointcloud_viewer/camera.cpp
+++ src/pointcloud_viewer/camera.cpp
@@ -1,5 +1,7 @@
 #include <pointcloud_viewer/camera.hpp>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 Camera::Camera()
 {
 }
--- src/pointcloud_viewer/camera.hpp
+++ src/pointcloud_viewer/camera.hpp
@@ -1,11 +1,17 @@
 #ifndef POINTCLOUDVIEWER_CAMERA_HPP_
 #define POINTCLOUDVIEWER_CAMERA_HPP_
 
-#include <glm/glm.hpp>
+#include <geometry/frame.hpp>
 
 class Camera final
 {
 public:
+  frame_t frame = frame_t(glm::vec3(7.481132f, -6.50764f, 5.343665f),
+                          glm::quat(0.7816f, 0.481707f, 0.212922f, 0.334251f));
+  float fov_y = glm::radians(90.f);
+  float near = 1.e-2f;
+  float far = 100.f;
+
   Camera();
   ~Camera();
 };
