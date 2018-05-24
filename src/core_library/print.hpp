c15457e37036b7b98bc8220b2e0d72eff4eb5de4
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-05-24 15:47:17 +0200

    added print macro print values

--- src/core_library/print.hpp
+++ src/core_library/print.hpp
@@ -12,6 +12,8 @@ Helper functions for operating with strings.
     format("answer to the life, universe and everything: ", 42, ". Deep inside, you know it's true!")
 
 `print` do the same, but they already prints the result to the console.
+
+`PRINT` is a macro printing an expression along with the expression itself. For example PRINT(6*7) prints `6*7: 42`
 */
 
 template<typename... arg_t>
@@ -22,6 +24,8 @@ std::ostream& print(const arg_t&... args);
 
 template<typename... arg_t>
 std::ostream& print_error(const arg_t&... args);
+
+#define PRINT(expression) print(#expression ": ", expression)
 
 #include <core_library/print.inl>
 
