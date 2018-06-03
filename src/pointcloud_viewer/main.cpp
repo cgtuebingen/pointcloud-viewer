112f2d58f2fc370b22255f17c04782fa2a3ec10e
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-06-03 09:26:40 +0200

    commented out the first tinyply test

--- src/pointcloud_viewer/main.cpp
+++ src/pointcloud_viewer/main.cpp
@@ -8,13 +8,15 @@
 
 int main(int argc, char** argv)
 {
+  /*
   tinyply::PlyFile file;
 
   std::ifstream filestream("/home/hildebrr/Desktop/area1_rgb.ply", std::ios::in);
   file.parse_header(filestream);
   file.request_properties_from_element("vertex", {"x", "y", "z"});
-  // TODO I have th efull contoll over the stream! I can copy blocks of the ply file to the memory and read them with file.read? Can I? THis way, I can have a progress bar!! :D
+  // TODO I have the full contoll over the stream! I can copy blocks of the ply file to the memory and read them with file.read? Can I? THis way, I can have a progress bar!! :D
   // file.read(filestream);
+  */
 
   QApplication application(argc, argv);
 
