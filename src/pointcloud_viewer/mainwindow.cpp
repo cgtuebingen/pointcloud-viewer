85016f4e3ae64e1fff45db7a0c25a138f4014fe7
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-07-22 20:52:21 +0200

    continued handling arguments

--- src/pointcloud_viewer/mainwindow.cpp
+++ src/pointcloud_viewer/mainwindow.cpp
@@ -1,8 +1,5 @@
 #include <pointcloud_viewer/mainwindow.hpp>
 
-#include <QApplication>
-#include <QDebug>
-
 MainWindow::MainWindow()
   : pointCloudLayer(this)
 {
@@ -20,61 +17,4 @@ MainWindow::MainWindow()
 
 MainWindow::~MainWindow()
 {
-}
-
-void MainWindow::handleApplicationArguments()
-{
-  const QStringList arguments = qApp->arguments();
-
-  for(int argument_index=1; argument_index<arguments.length(); ++argument_index)
-  {
-    const QString argument = arguments[argument_index];
-
-    if(argument == "--background")
-    {
-      // TODO
-    }else if(argument == "--data")
-    {
-      if(argument_index+1 == arguments.length())
-      {
-        qDebug() << "Missing argument after \"--data\"";
-        std::exit(-1);
-      }
-      // TODO
-    }else if(argument == "--path")
-    {
-      if(argument_index+1 == arguments.length())
-      {
-        qDebug() << "Missing argument after \"--path\"";
-        std::exit(-1);
-      }
-
-      // TODO
-    }else if(argument == "--output_dir")
-    {
-      if(argument_index+1 == arguments.length())
-      {
-        qDebug() << "Missing argument after \"--output_dir\"";
-        std::exit(-1);
-      }
-
-      // TODO
-    }else if(argument == "--help")
-    {
-      qDebug() << "Usage: pointcloud_viewer [ARGUMENTS]\n"
-                  "                                                                                \n"
-                  "--help             Show this helptext and exits immediately                     \n"
-                  "--background       Hides the gui. Used for using this software as command-line  \n"
-                  "                   tool.                                                        \n"
-                  "--data <FILE>      Pointcloud file to load                                      \n"
-                  "--path <FILE>      The path of the camera                                       \n"
-                  "--output_dir <DIR> Where to save the rendered files                             \n"
-                  ;
-      std::exit(0);
-    }else
-    {
-      qDebug() << "Unexpected argument " << argument;
-      std::exit(-1);
-    }
-  }
 }
---
+++ src/pointcloud_viewer/mainwindow_arguments.cpp
@@ -0,0 +1,72 @@
+#include <pointcloud_viewer/mainwindow.hpp>
+#include <pointcloud_viewer/workers/import_pointcloud.hpp>
+
+#include <QApplication>
+#include <QDebug>
+
+void MainWindow::handleApplicationArguments()
+{
+  const QStringList arguments = qApp->arguments();
+
+  for(int argument_index=1; argument_index<arguments.length(); ++argument_index)
+  {
+    const QString argument = arguments[argument_index];
+
+    if(argument == "--background")
+    {
+      // TODO
+    }else if(argument == "--data")
+    {
+      if(argument_index+1 == arguments.length())
+      {
+        qDebug() << "Missing argument after \"--data\"";
+        std::exit(-1);
+      }
+      argument_index++;
+
+      const QString path = arguments[argument_index];
+
+      viewport.load_point_cloud(import_point_cloud(this, path));
+    }else if(argument == "--camera-path")
+    {
+      if(argument_index+1 == arguments.length())
+      {
+        qDebug() << "Missing argument after \"--path\"";
+        std::exit(-1);
+      }
+      argument_index++;
+
+      const QString path = arguments[argument_index];
+
+      flythrough.import_path(path);
+    }else if(argument == "--output_dir")
+    {
+      if(argument_index+1 == arguments.length())
+      {
+        qDebug() << "Missing argument after \"--output_dir\"";
+        std::exit(-1);
+      }
+      argument_index++;
+
+      const QString path = arguments[argument_index];
+
+      renderSettings.target_images_directory = QDir(path).absolutePath();
+    }else if(argument == "--help")
+    {
+      qDebug() << "Usage: pointcloud_viewer [ARGUMENTS]\n"
+                  "\n"
+                  "--help               Show this helptext and exits immediately                   \n"
+                  "--background         Hides the gui. Used for using this software as command-line\n"
+                  "                     tool.                                                      \n"
+                  "--data <FILE>        Pointcloud file to load                                    \n"
+                  "--camera-path <FILE> The path of the camera                                     \n"
+                  "--output_dir <DIR>   Where to save the rendered files                           \n"
+                  ;
+      std::exit(0);
+    }else
+    {
+      qDebug() << "Unexpected argument " << argument;
+      std::exit(-1);
+    }
+  }
+}
