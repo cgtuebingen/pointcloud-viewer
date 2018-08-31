b155e717c7785cae9d3f60e23d30e9b55edbcbb0
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-08-31 11:56:19 +0200

    added frame for the pcvd imported

---
+++ src/pointcloud/importer/pcvd_importer.cpp
@@ -0,0 +1,11 @@
+#include <pointcloud/importer/pcvd_importer.hpp>
+
+PcvdImporter::PcvdImporter(const std::string& input_file)
+  : AbstractPointCloudImporter(input_file)
+{
+}
+
+bool PcvdImporter::import_implementation()
+{
+  // TODO
+}
---
+++ src/pointcloud/importer/pcvd_importer.hpp
@@ -0,0 +1,21 @@
+#ifndef POINTCLOUD_WORKERS_IMPORTER_PLY_HPP_
+#define POINTCLOUD_WORKERS_IMPORTER_PLY_HPP_
+
+#include <pointcloud/importer/abstract_importer.hpp>
+#include <pointcloud/buffer.hpp>
+
+#include <QVector>
+
+/**
+Implementation for loading ply files
+*/
+class PcvdImporter final : public AbstractPointCloudImporter
+{
+public:
+  PcvdImporter(const std::string& input_file);
+
+protected:
+  bool import_implementation() override;
+};
+
+#endif // POINTCLOUD_WORKERS_IMPORTER_PLY_HPP_
