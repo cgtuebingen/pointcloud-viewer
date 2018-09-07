c3b075637ce6f0c20ea80009dbc9f8377a431527
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-09-07 12:07:49 +0200

    disabled  wasting space for vertex buffers

--- src/pointcloud/exporter/pcvd_exporter.hpp
+++ src/pointcloud/exporter/pcvd_exporter.hpp
@@ -9,7 +9,7 @@ public:
   PcvdExporter(const std::string& output_file, const PointCloud& pointcloud);
 
   bool save_kd_tree = true;
-  bool save_vertex_data = true;
+  bool save_vertex_data = false;
 
 protected:
   bool export_implementation() override;
