2a6093e4499e6938f70fa811d80f6ae671b0a51c
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-09-07 12:57:55 +0200

    finetuned picking radius adaption

--- src/pointcloud_viewer/pointcloud_inspector.cpp
+++ src/pointcloud_viewer/pointcloud_inspector.cpp
@@ -57,13 +57,13 @@ void PointCloudInspector::pick_point(glm::ivec2 pixel)
   if(!point_cloud || !point_cloud->has_build_kdtree())
     return;
 
-  float pick_radius = m_pickRadius * 2.f;
+  float pick_radius = glm::max(4.f, glm::ceil(m_pickRadius + 2.f));
   glm::ivec2 viewport_size(viewport.width(), viewport.height());
 
   glm::vec2 screenspace_point = Camera::pixel_to_screenspace(pixel, viewport_size);
 
   ray_t center_ray = viewport.navigation.camera.ray_for_screenspace_point(screenspace_point);
-  ray_t side_ray = viewport.navigation.camera.ray_for_screenspace_point(glm::mix(screenspace_point, Camera::pixel_to_screenspace(pixel+glm::ivec2(1, 0), viewport_size), pick_radius));
+  ray_t side_ray = viewport.navigation.camera.ray_for_screenspace_point(Camera::pixel_to_screenspace(pixel+glm::ivec2(pick_radius, 0), viewport_size));
 
   float angle = glm::acos(glm::dot(center_ray.direction, side_ray.direction));
 
