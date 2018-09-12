c52bb0134a65578775b310d331c3536ce39e9532
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-09-12 10:58:43 +0200

    prepared switching between two schemes

--- src/pointcloud_viewer/usability_scheme.cpp
+++ src/pointcloud_viewer/usability_scheme.cpp
@@ -1,43 +1,210 @@
 #include <pointcloud_viewer/usability_scheme.hpp>
 
 
+class UsabilityScheme::Implementation::BlenderScheme final : public UsabilityScheme::Implementation
+{
+public:
+  BlenderScheme();
+
+  void on_enable() override;
+  void on_disable() override;
+
+  void wheelEvent(QWheelEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseReleaseEvent(QMouseEvent* event) override;
+  void keyPressEvent(QKeyEvent* event) override;
+  void keyReleaseEvent(QKeyEvent* event) override;
+  void focusOutEvent(QFocusEvent* event) override;
+};
+
+class UsabilityScheme::Implementation::MeshLabScheme final : public UsabilityScheme::Implementation
+{
+public:
+  MeshLabScheme();
+
+  void on_enable() override;
+  void on_disable() override;
+
+  void wheelEvent(QWheelEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseReleaseEvent(QMouseEvent* event) override;
+  void keyPressEvent(QKeyEvent* event) override;
+  void keyReleaseEvent(QKeyEvent* event) override;
+  void focusOutEvent(QFocusEvent* event) override;
+};
+
+
+UsabilityScheme::UsabilityScheme()
+{
+  implementations[BLENDER] = QSharedPointer<Implementation>(new Implementation::BlenderScheme);
+  implementations[MESHLAB] = QSharedPointer<Implementation>(new Implementation::MeshLabScheme);
+
+  enableBlenderScheme();
+}
+
 UsabilityScheme::~UsabilityScheme()
 {
+  if(_implementation != nullptr)
+    _implementation->on_disable();
+}
 
+void UsabilityScheme::enableBlenderScheme()
+{
+  enableScheme(BLENDER);
+}
+
+void UsabilityScheme::enableMeshlabScheme()
+{
+  enableScheme(MESHLAB);
+}
+
+void UsabilityScheme::enableScheme(scheme_t scheme)
+{
+  if(_implementation != nullptr)
+    _implementation->on_disable();
+
+  _implementation = implementations.value(scheme, implementations.value(BLENDER)).data();
+
+  if(_implementation != nullptr)
+    _implementation->on_enable();
 }
 
 void UsabilityScheme::wheelEvent(QWheelEvent* event)
 {
-  implementation->wheelEvent(event);
+  _implementation->wheelEvent(event);
 }
 
 void UsabilityScheme::mouseMoveEvent(QMouseEvent* event)
 {
 
-  implementation->mouseMoveEvent(event);
+  _implementation->mouseMoveEvent(event);
 }
 
 void UsabilityScheme::mousePressEvent(QMouseEvent* event)
 {
-  implementation->mousePressEvent(event);
+  _implementation->mousePressEvent(event);
 }
 
 void UsabilityScheme::mouseReleaseEvent(QMouseEvent* event)
 {
-  implementation->mouseReleaseEvent(event);
+  _implementation->mouseReleaseEvent(event);
 }
 
 void UsabilityScheme::keyPressEvent(QKeyEvent* event)
 {
-  implementation->keyPressEvent(event);
+  _implementation->keyPressEvent(event);
 }
 
 void UsabilityScheme::keyReleaseEvent(QKeyEvent* event)
 {
-  implementation->keyReleaseEvent(event);
+  _implementation->keyReleaseEvent(event);
 }
 
 void UsabilityScheme::focusOutEvent(QFocusEvent* event)
 {
-  implementation->focusOutEvent(event);
+  _implementation->focusOutEvent(event);
+}
+
+// ==== Blender ====
+
+UsabilityScheme::Implementation::BlenderScheme::BlenderScheme()
+{
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::on_enable()
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::on_disable()
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::wheelEvent(QWheelEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::mouseMoveEvent(QMouseEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::mousePressEvent(QMouseEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::mouseReleaseEvent(QMouseEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::keyPressEvent(QKeyEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::keyReleaseEvent(QKeyEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::BlenderScheme::focusOutEvent(QFocusEvent* event)
+{
+
+}
+
+// ==== Meshlab ====
+
+UsabilityScheme::Implementation::MeshLabScheme::MeshLabScheme()
+{
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::on_enable()
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::on_disable()
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::wheelEvent(QWheelEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::mouseMoveEvent(QMouseEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::mousePressEvent(QMouseEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::mouseReleaseEvent(QMouseEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::keyPressEvent(QKeyEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::keyReleaseEvent(QKeyEvent* event)
+{
+
+}
+
+void UsabilityScheme::Implementation::MeshLabScheme::focusOutEvent(QFocusEvent* event)
+{
+
 }
--- src/pointcloud_viewer/usability_scheme.hpp
+++ src/pointcloud_viewer/usability_scheme.hpp
@@ -2,15 +2,27 @@
 #define POINTCLOUDVIEWER_USABILILTY_SCHEME_HPP_
 
 #include <QMouseEvent>
+#include <QSharedPointer>
+#include <QVector>
+
+// Different Usability schemes on how to navigate the 3d space
 
 class UsabilityScheme final : public QObject
 {
 public:
-  class Implementation;
+  enum scheme_t
+  {
+    BLENDER = 0,
+    MESHLAB = 1,
+  };
 
-  Implementation* implementation;
-
+  UsabilityScheme();
   ~UsabilityScheme();
+
+  void enableBlenderScheme();
+  void enableMeshlabScheme();
+
+  void enableScheme(scheme_t scheme);
 
   void wheelEvent(QWheelEvent* event);
   void mouseMoveEvent(QMouseEvent* event);
@@ -19,12 +31,25 @@ public:
   void keyPressEvent(QKeyEvent* event);
   void keyReleaseEvent(QKeyEvent* event);
   void focusOutEvent(QFocusEvent* event);
+
+private:
+  class Implementation;
+
+  Implementation* _implementation = nullptr;
+
+  QMap<scheme_t, QSharedPointer<Implementation>> implementations;
 };
 
-class UsabilityScheme::Implementation : public QObject
+class UsabilityScheme::Implementation
 {
 public:
+  class BlenderScheme;
+  class MeshLabScheme;
+
   virtual ~Implementation() = default;
+
+  virtual void on_enable() = 0;
+  virtual void on_disable() = 0;
 
   virtual void wheelEvent(QWheelEvent* event) = 0;
   virtual void mouseMoveEvent(QMouseEvent* event) = 0;
@@ -34,5 +59,4 @@ public:
   virtual void keyReleaseEvent(QKeyEvent* event) = 0;
   virtual void focusOutEvent(QFocusEvent* event) = 0;
 };
-
 #endif // POINTCLOUDVIEWER_USABILILTY_SCHEME_HPP_
