92fce52cddfd612071922f40499f78e7eb811fc7
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-09-21 14:57:16 +0200

    now the camera is looking at the center

--- src/pointcloud_viewer/camera.hpp
+++ src/pointcloud_viewer/camera.hpp
@@ -3,6 +3,8 @@
 
 #include <geometry/frame.hpp>
 #include <geometry/ray.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 /*
 Object storing the camera parameters and returning the camera matrices
@@ -10,8 +12,8 @@ Object storing the camera parameters and returning the camera matrices
 class Camera final
 {
 public:
-  frame_t frame = frame_t(glm::vec3(7.481132f, -6.50764f, 5.343665f) * 0.25f,
-                          remove_tilt(glm::quat(0.7816f, 0.481707f, 0.212922f, 0.334251f)));
+  frame_t frame = frame_t(default_camera_position(),
+                          remove_tilt(glm::quatLookAt(glm::normalize(-default_camera_position()), glm::vec3(0,0,1))));
   float aspect = 1.f;
   float fov_y = glm::radians(90.f);
   float z_near = 1.e-3f;
@@ -29,6 +31,12 @@ public:
 
   static glm::vec2 screenspace_to_clipspace(glm::vec2 screenspace_point);
   static glm::vec2 pixel_to_screenspace(glm::ivec2 pixel, glm::ivec2 viewport_size);
+
+private:
+  static glm::vec3 default_camera_position()
+  {
+    return glm::vec3(7.5f, -6.5f, 5.3f) * 0.25f;
+  }
 };
 
 #endif // POINTCLOUDVIEWER_CAMERA_HPP_
