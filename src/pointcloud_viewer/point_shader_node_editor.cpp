b0c1826f0cfb441c9a1a64592d314b6d7e9c4c60
Robert Hildebrandt <robert.hildebrandt@bitractive.com>
2018-10-16 09:52:03 +0200

    added split vector node

--- src/pointcloud_viewer/point_shader_node_editor.cpp
+++ src/pointcloud_viewer/point_shader_node_editor.cpp
@@ -653,6 +653,140 @@ QWidget* MakeVectorNode::embeddedWidget()
   return nullptr;
 }
 
+// ==== Split Vector ================
+
+class SplitVectorNode final : public QtNodes::NodeDataModel
+{
+public:
+  SplitVectorNode();
+
+  QString caption() const override{return "SplitVector";}
+  QString name() const override{return "SplitVector";}
+  uint nPorts(QtNodes::PortType portType) const override;
+  QString portCaption(QtNodes::PortType, QtNodes::PortIndex) const override;
+  bool portCaptionVisible(QtNodes::PortType, QtNodes::PortIndex) const override;
+  QtNodes::NodeDataType dataType(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const override;
+
+  void setInData(std::shared_ptr<QtNodes::NodeData> nodeData, QtNodes::PortIndex port) override;
+
+  std::shared_ptr<QtNodes::NodeData> outData(QtNodes::PortIndex port) override;
+
+  QWidget* embeddedWidget() override;
+
+private:
+  std::shared_ptr<Value> vector;
+  std::shared_ptr<Value> x, y, z;
+};
+
+SplitVectorNode::SplitVectorNode()
+{
+  vector = std::make_shared<Value>("ivec3(0,0,0)", VALUE_TYPE::IVEC3);
+  x = std::make_shared<Value>("0", VALUE_TYPE::INT);
+  y = std::make_shared<Value>("0", VALUE_TYPE::INT);
+  z = std::make_shared<Value>("0", VALUE_TYPE::INT);
+}
+
+uint SplitVectorNode::nPorts(QtNodes::PortType portType) const
+{
+  switch(portType)
+  {
+  case QtNodes::PortType::In:
+    return 1;
+  case QtNodes::PortType::Out:
+    return 3;
+  case QtNodes::PortType::None:
+    return 0;
+  }
+  return 0;
+}
+
+QString SplitVectorNode::portCaption(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const
+{
+  switch(portType)
+  {
+  case QtNodes::PortType::Out:
+    if(portIndex == 0)
+      return "X";
+    else if(portIndex == 1)
+      return "Y";
+    else if(portIndex == 2)
+      return "Z";
+    break;
+  case QtNodes::PortType::In:
+    return "Vector";
+  case QtNodes::PortType::None:
+    break;
+  }
+
+  Q_UNREACHABLE();
+  return "";
+}
+
+bool SplitVectorNode::portCaptionVisible(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const
+{
+  switch(portType)
+  {
+  case QtNodes::PortType::In:
+    return portIndex == 0;
+  case QtNodes::PortType::Out:
+    return portIndex>=0 && portIndex < 3;
+  case QtNodes::PortType::None:
+    break;
+  }
+
+  return false;
+}
+
+QtNodes::NodeDataType SplitVectorNode::dataType(QtNodes::PortType, QtNodes::PortIndex) const
+{
+  return Value().type();
+}
+
+void SplitVectorNode::setInData(std::shared_ptr<QtNodes::NodeData> nodeData, QtNodes::PortIndex portIndex)
+{
+  Q_ASSERT(portIndex == 0);
+
+  if(nodeData == nullptr)
+    vector = std::make_shared<Value>("ivec3(0,0,0)", VALUE_TYPE::IVEC3);
+  else
+    vector = std::dynamic_pointer_cast<Value>(nodeData);
+
+  const value_type_t scalar_type = to_scalar(vector->value_type);
+
+  if(is_vector(vector->value_type))
+  {
+    x = std::make_shared<Value>(QString("(%0).x").arg(vector->expression), scalar_type);
+    y = std::make_shared<Value>(QString("(%0).y").arg(vector->expression), scalar_type);
+    z = std::make_shared<Value>(QString("(%0).z").arg(vector->expression), scalar_type);
+  }else
+  {
+    x = vector;
+    y = vector;
+    z = vector;
+  }
+
+  dataUpdated(0);
+  dataUpdated(1);
+  dataUpdated(2);
+}
+
+std::shared_ptr<QtNodes::NodeData> SplitVectorNode::outData(QtNodes::PortIndex portIndex)
+{
+  Q_ASSERT(portIndex == 0 || portIndex == 1 || portIndex == 2);
+
+  if(portIndex == 0)
+    return x;
+  else if(portIndex == 1)
+    return y;
+  else
+    return z;
+}
+
+QWidget* SplitVectorNode::embeddedWidget()
+{
+  return nullptr;
+}
+
 // ==== PointShader::edit ================
 
 void PointShader::edit(QWidget* parent, const QSharedPointer<PointCloud>& currentPointcloud)
@@ -691,7 +825,7 @@ void PointShader::edit(QWidget* parent, const QSharedPointer<PointCloud>& curren
       base_type_for_name[currentPointcloud->user_data_names[i]] = currentPointcloud->user_data_types[i];
   }
 
-  if(supportedPropertyNames.isEmpty())
+  if(supportedPropertyNames.isEmpty() && missingPropertyNames.isEmpty())
   {
     missingPropertyNames << "x" << "y" << "z";
     base_type_for_name["x"]   = base_type_for_name["y"]    = base_type_for_name["z"]     = PROPERTY_TYPE::FLOAT32;
@@ -702,6 +836,7 @@ void PointShader::edit(QWidget* parent, const QSharedPointer<PointCloud>& curren
   std::shared_ptr<QtNodes::DataModelRegistry> registry(new QtNodes::DataModelRegistry);
 
   registry->registerModel<MakeVectorNode>("Vector");
+  registry->registerModel<SplitVectorNode>("Vector");
   registry->registerModel<OutputNode>("Output");
   registry->registerModel<SpyNode>("Output");
   registry->registerModel<PropertyNode>("Input",
